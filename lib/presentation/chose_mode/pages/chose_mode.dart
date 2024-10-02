473c8d0d36752fd3b2da1e4ecba00dd06c1ab8ac
ThiagoC0STA <thiago@lacostacorretora.com.br>
2024-10-02 12:25:17 -0300

    chose mode logic

--- lib/presentation/chose_mode/pages/chose_mode.dart
+++ lib/presentation/chose_mode/pages/chose_mode.dart
@@ -1,11 +1,13 @@
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:spotify_app/common/widgets/button/basic_app_button.dart';
 import 'package:spotify_app/core/configs/assets/app_images.dart';
 import 'package:spotify_app/core/configs/assets/app_vectors.dart';
 import 'package:spotify_app/core/configs/theme/app_colors.dart';
+import 'package:spotify_app/presentation/chose_mode/bloq/theme_cubit.dart';
 
 class ChoseModePage extends StatelessWidget {
   const ChoseModePage({super.key});
@@ -40,74 +42,86 @@ class ChoseModePage extends StatelessWidget {
                   crossAxisAlignment: CrossAxisAlignment.center,
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        ClipOval(
-                          child: BackdropFilter(
-                            filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
-                            child: Container(
-                              height: 73,
-                              width: 73,
-                              decoration: BoxDecoration(
-                                color: const Color(0xff30393c).withOpacity(0.6),
-                                shape: BoxShape.circle,
-                              ),
-                              child: SvgPicture.asset(
-                                AppVectors.moon,
-                                fit: BoxFit.none,
+                    GestureDetector(
+                      onTap: () {
+                        context.read<ThemeCubit>().updateTheme(ThemeMode.dark);
+                      },
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          ClipOval(
+                            child: BackdropFilter(
+                              filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
+                              child: Container(
+                                height: 73,
+                                width: 73,
+                                decoration: BoxDecoration(
+                                  color:
+                                      const Color(0xff30393c).withOpacity(0.6),
+                                  shape: BoxShape.circle,
+                                ),
+                                child: SvgPicture.asset(
+                                  AppVectors.moon,
+                                  fit: BoxFit.none,
+                                ),
                               ),
                             ),
                           ),
-                        ),
-                        const SizedBox(
-                          height: 20,
-                        ),
-                        const Text(
-                          'Dark Mode',
-                          style: TextStyle(
-                              color: AppColors.grey,
-                              fontSize: 17,
-                              fontWeight: FontWeight.bold),
-                        )
-                      ],
+                          const SizedBox(
+                            height: 20,
+                          ),
+                          const Text(
+                            'Dark Mode',
+                            style: TextStyle(
+                                color: AppColors.grey,
+                                fontSize: 17,
+                                fontWeight: FontWeight.bold),
+                          )
+                        ],
+                      ),
                     ),
                     const SizedBox(
                       width: 70,
                     ),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        ClipOval(
-                          child: BackdropFilter(
-                            filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
-                            child: Container(
-                              height: 73,
-                              width: 73,
-                              decoration: BoxDecoration(
-                                color: const Color(0xff30393c).withOpacity(0.6),
-                                shape: BoxShape.circle,
-                              ),
-                              child: SvgPicture.asset(
-                                AppVectors.sun,
-                                fit: BoxFit.none,
+                    GestureDetector(
+                      onTap: () {
+                        context.read<ThemeCubit>().updateTheme(ThemeMode.light);
+                      },
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          ClipOval(
+                            child: BackdropFilter(
+                              filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
+                              child: Container(
+                                height: 73,
+                                width: 73,
+                                decoration: BoxDecoration(
+                                  color:
+                                      const Color(0xff30393c).withOpacity(0.6),
+                                  shape: BoxShape.circle,
+                                ),
+                                child: SvgPicture.asset(
+                                  AppVectors.sun,
+                                  fit: BoxFit.none,
+                                ),
                               ),
                             ),
                           ),
-                        ),
-                        const SizedBox(
-                          height: 20,
-                        ),
-                        const Text(
-                          'Light Mode',
-                          style: TextStyle(
-                              color: AppColors.grey,
-                              fontSize: 17,
-                              fontWeight: FontWeight.bold),
-                        )
-                      ],
+                          const SizedBox(
+                            height: 20,
+                          ),
+                          const Text(
+                            'Light Mode',
+                            style: TextStyle(
+                                color: AppColors.grey,
+                                fontSize: 17,
+                                fontWeight: FontWeight.bold),
+                          )
+                        ],
+                      ),
                     ),
                   ],
                 ),
